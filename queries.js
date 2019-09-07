84c49d19cdd19677c4747776d99f4b4170f8f06b
Jordan Schactman <jordanschactman@ufl.edu>
2019-09-07 16:07:06 -0400

    Implement queries

--- queries.js
+++ queries.js
@@ -1,6 +1,15 @@
 /* Add all the required libraries*/
 
+var mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+
 /* Connect to your database using mongoose - remember to keep your key secret*/
+
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
@@ -10,6 +19,17 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  
+  Listing.findOne({ name: 'Library West' }, function(err, listing) {
+
+    if (err) {
+      throw err;
+    }
+
+    console.log(listing);
+
+  });
+
 };
 var removeCable = function() {
   /*
@@ -17,20 +37,51 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+
+   Listing.findOneAndDelete({ code: 'CABL' }, function(err, listing) {
+
+     if (err) {
+       throw err;
+     }
+
+     console.log(listing);
+
+   });
+
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Lab address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+
+   Listing.findOneAndUpdate({ name: 'Phelps Laboratory' }, { address: '102 Phelps Lab, Gainesville, FL 32611, United States' }, { new: true }, function(err, listing) {
+
+     if (err) {
+       throw err;
+     }
+
+     console.log(listing);
+
+   });
+
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
+  Listing.find({}, function(err, listings) {
+
+    listings.forEach(function(listing) {
+      console.log(listing);
+    });
+
+  });
+
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
