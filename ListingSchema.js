03ab577fb55e6f17fedc1d8d26ebb2fe71c60f41
Jordan Schactman <jordanschactman@ufl.edu>
2019-09-07 15:33:15 -0400

    Added listing schema

--- ListingSchema.js
+++ ListingSchema.js
@@ -9,7 +9,15 @@ var mongoose = require('mongoose'),
 var listingSchema = new Schema({
   /* Your code for a schema here */ 
   //Check out - https://mongoosejs.com/docs/guide.html
-
+  code: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  coordinates: {
+  	latitude: Number,
+  	longitude: Number
+  },
+  address: String,
+  created_at: Date,
+  updated_at: Date
 });
 
 /* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
@@ -17,6 +25,19 @@ var listingSchema = new Schema({
 */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at) {
+  	this.created_at = currentDate;
+  }
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
